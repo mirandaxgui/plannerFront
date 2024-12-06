d12470a33914f8fb062cf464485643521e174d9d
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-06 02:44:24 -0300

    Implementing Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+  import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://18.230.184.125:8080',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+  export default defineConfig({
+    base: '/plannerApi/',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://18.230.184.125:8080',
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-});
+  });
