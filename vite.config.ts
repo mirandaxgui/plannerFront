286f9f49d6c5b32eba78ae6bcae45aaea0b69375
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-06 03:02:55 -0300

    Implementing Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/plannerApi/',  
+  base: '/plannerFront/',  
   plugins: [react()],
   server: {
     proxy: {
