24eb29f4e089a987da5b907ea1f31759c022c0c4
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-10 15:02:41 -0300

    Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/plannerFront/',  
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://18.230.184.125:8080',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  base: '/plannerFront/', // Deve corresponder ao "homepage" no package.json
 });
