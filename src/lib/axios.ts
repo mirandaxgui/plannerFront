e623e2e04df09c6b1c8253191c45255003d73be2
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-11 14:35:54 -0300

    Trying VITE

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: import.meta.env.MODE === 'production'
     ? 'http://18.230.184.125:8080'  // URL do seu backend em produção
-    : '/api',  // URL de proxy para desenvolvimento
+    : 'http://18.230.184.125:8080',  // URL de proxy para desenvolvimento
   withCredentials: true,
 });
