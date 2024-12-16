951308ea70dc0f94dd650f23e175c5703e93db32
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-16 11:26:12 -0300

    IDK

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,4 @@
-import {
-  HashRouter as Router, // Usando HashRouter para GitHub Pages
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { CreateTripPage } from "./pages/create-trip";
 import { TripDetailsPage } from "./pages/trip-details";
 import { LoginPage } from "./pages/login";
@@ -15,17 +10,12 @@ export function App() {
     <Router basename="/plannerFront"> {/* Base path para GitHub Pages */}
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        
         <Route path="/login" element={<LoginPage />} />
-        
         <Route path="/participant/register" element={<RegisterStep />} />
-        
         <Route path="/trips" element={<PrivateRoute />}>
           <Route path="/trips" element={<CreateTripPage />} />
           <Route path="/trips/:tripId" element={<TripDetailsPage />} />
         </Route>
-        
-        {/* Aqui, qualquer URL não definida levará para a página de login */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
