d4c595e2963d6576a3103306f871329658170dd7
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-16 12:29:33 -0300

    Z

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,5 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { LoginPage } from './pages/login';
-import { CreateTripPage } from './pages/create-trip';
-import { TripDetailsPage } from './pages/trip-details';
 
 export function App() {
   return (
@@ -9,8 +7,6 @@ export function App() {
       <Routes>
         <Route path="/" element={<div>Home Page</div>} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/trips" element={<CreateTripPage />} />
-        <Route path="/trips/:tripId" element={<TripDetailsPage />} />
         <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
     </Router>
